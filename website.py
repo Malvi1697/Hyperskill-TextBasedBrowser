c1901062914c51b61823e06fab47cf1a2be5e864
Malvi1697 <129746042+Malvi1697@users.noreply.github.com>
2024-05-15 17:50:41 +0200

    Init

---
+++ website.py
@@ -0,0 +1,27 @@
+import os
+class Website:
+    history = []
+    def __init__(self, webpage_url, webpages_list, dir_name):
+        self.dir_name = dir_name
+        self.webpage_url = webpage_url
+        self.webpages_list = webpages_list
+
+
+    def stripped(self):
+        return self.webpage_url.split(".")[0]
+
+    def mktab(self):
+        try:
+            os.mkdir(self.dir_name)
+        except FileExistsError:
+            print("File already exists")
+
+        with open(self.dir_name + "/" + self.stripped(), "w") as url_text_file:
+            url_text_file.write(self.__str__())
+
+    def __str__(self):
+        for webpage in self.webpages_list:
+            if self.webpage_url in webpage:
+                webpage = self.webpages_list[(self.webpages_list.index(webpage)) + 1]
+                break
+        return webpage
